c0cbd8baac0115e7955a4551d298f80ecab25527
escapade64 <86234015+escapade64@users.noreply.github.com>
2026-07-14 16:24:58 +0200

    Ajouter le spot Pereire - Arcachon

--- frontend/config.js
+++ frontend/config.js
@@ -17,6 +17,17 @@ const SPOTS = [
       tolerance: 30,
     },
   },
+  {
+    id: "pereire",
+    name: "Péreire - Arcachon",
+    lat: 44.649916465296805,
+    lon: -1.2015675382325726,
+    criteria: {
+      min_speed: 15,
+      direction_center: 250,
+      tolerance: 20,
+    },
+  },
 ];
 
 function getSpot(spotId) {
